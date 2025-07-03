7ae34854edd3ab4bf3118ae4d34d940091af621b
Max Basev <64890011+MaxBasev@users.noreply.github.com>
2025-07-03 11:47:40 +0600

    GTM add

--- nextjs-portfolio/src/app/layout.tsx
+++ nextjs-portfolio/src/app/layout.tsx
@@ -57,6 +57,15 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				{/* Google Tag Manager */}
+				<script dangerouslySetInnerHTML={{
+					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-TF8WVR6Q');`
+				}} />
+
 				{/* Favicons */}
 				<link rel="icon" href="/MB_Logo.png" type="image/png" />
 				<link rel="apple-touch-icon" href="/MB_Logo.png" />
@@ -184,7 +193,18 @@ export default function RootLayout({
 				}} />
 				*/}
 			</head>
-			<body>{children}</body>
+			<body>
+				{/* Google Tag Manager (noscript) */}
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-TF8WVR6Q"
+						height="0"
+						width="0"
+						style={{ display: 'none', visibility: 'hidden' }}
+					/>
+				</noscript>
+				{children}
+			</body>
 		</html>
 	)
 }
